d89895b0d620a6a8d71d575b2f39e9c42d283aa6
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-03-19 14:26:06 +0100

    Removing double quotemarks from code

--- contracts/CustomDealToken.sol
+++ contracts/CustomDealToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.11;
 import './token/MintableToken.sol';
 
 contract CustomDealToken is MintableToken {
-    string public constant name = "Custom Deal Token";
-    string public constant symbol = "CDL";
+    string public constant name = 'Custom Deal Token';
+    string public constant symbol = 'CDL';
     uint256 public constant decimals = 18;
     uint256 public constant _totalSupply = 400000000 * 1 ether;
 
