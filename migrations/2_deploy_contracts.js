7e483e18b90be13987b7b863e0b2d560b8b47952
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-06-07 18:00:52 +0200

    Custom Deal Token and Crowdsale Smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ module.exports = function (deployer) {
         startTime,
         endTime,
         40000,
-        '0x1f0C69e1Bc6c8F7d30f163d89De69b88C6A22a30', // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address.
+        '0xa2d79C849b115F23e6615272C474ee695Ee493E8', // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address.
         10000000000000000000000, // 10000 ETH - Soft-cap
         60000000000000000000000 // 60000 ETH - Hard-cap
     );
