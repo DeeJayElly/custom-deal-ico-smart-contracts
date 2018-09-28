c71a9bbdec2784bf285bf6bdd6d5621bff50c202
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-09-28 13:00:49 +0200

    Custom Deal Token and Crowdsale Smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-let CustomDealICO = artifacts.require('./CustomDealICO.sol');
+const CustomDealICO = artifacts.require('./CustomDealICO.sol');
 
 module.exports = function (deployer) {
     deployer.deploy(CustomDealICO,
         1528705800,
-        1535846340,
+        1540943995,
         40000,
-        '0xa2d79C849b115F23e6615272C474ee695Ee493E8', // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address.
+        '0xa2d79C849b115F23e6615272C474ee695Ee493E8', // Beneficiary address.
         10000000000000000000,
         30000000000000000000000 // 30000 ETH - Hard-cap
     );
